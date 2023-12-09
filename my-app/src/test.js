5e41e4d44023b89b504389baee72f0d66a954517
unknown <aleksandarstanojevic93@gmail.com>
2023-12-09 17:38:00 +0100

    React and useless pdf removal

--- my-app/src/test.js
+++
@@ -1,7 +0,0 @@
-let message = () => {
-    let name = 'Aleksandar';
-    let surname = 'Stanojevic';
-    let age = 25;
-    return 'My name is ' + name + ' ' + surname + ' and I am ' + age + ' years old.';
-}
-export default message;
